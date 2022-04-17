b745f89921f3667b329e06b0cf24ab78f84b13ba
Aditya singh <adityas9027@gmail.com>
2022-04-18 00:53:58 +0530

    one to one chat

--- server/server.js
+++ server/server.js
@@ -11,9 +11,34 @@ const io = socketIO(server);
 app.use(express.static(path.join(__dirname, '../public')))
 
 io.on('connection', (socket) => {
+    // console.log(socket);
     console.log('New Connection..');
-})
 
+    // socket.emit('newMessage', {
+    //     from: 'jen@mds',
+    //     text: 'hepppp',
+    //     createdAt: 123
+    // });
+
+    // // listen for message from user
+    // socket.on('createMessage', (newMessage) => {
+    //     console.log('newMessage', newMessage);
+    // });
+
+    // socket.broadcast.emit('hi');
+
+    // socket.on('chat message', (msg) => {
+    //     console.log('message: ' + msg, ` ${socket.id}`);
+    // });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from user');
+    });
+})
 
 const port = 3000 || process.env.PORT;
 
